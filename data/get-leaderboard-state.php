3f441eee0f9549632183af2eb29317e30f981886
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-03-28 18:48:00 +0000

    Made limit parameter optional

--- data/get-leaderboard-state.php
+++ data/get-leaderboard-state.php
@@ -9,10 +9,10 @@ $request = json_decode(file_get_contents("php://input"), true);
 
 if ($request === null) $request = [];
 
-if (!array_key_exists("orderBy", $request) || !array_key_exists("limit", $request)) {
+if (!array_key_exists("orderBy", $request)) {
     die(json_encode([
         "status" => "fail",
-        "message" => "You must specify orderBy and limit."
+        "message" => "You must specify orderBy."
     ]));
 }
 else {
@@ -24,6 +24,10 @@ else {
     }
 }
 
+if (array_key_exists("limit", $request)) {
+    $LIMIT = $request["limit"];
+}
+
 $conn = new mysqli("localhost:3306", "Data", "", "DataCat");
 
 if ($conn->connect_error) {
@@ -33,8 +37,9 @@ if ($conn->connect_error) {
 if (array_key_exists("leaderboardId", $request)) {
     try {
         mysqli_report(MYSQLI_REPORT_ALL);
-        $stmt = $conn->prepare("SELECT SSO.Users.username AS 'username', SSO.Users.fullName AS 'fullName', SSO.Users.pfpPath AS 'pfpPath', `value` FROM Leaderboards_Data JOIN SSO.Users ON `user` = SSO.Users.id WHERE leaderboard = ? ORDER BY `value` " . $request["orderBy"] . " LIMIT ?");
-        $stmt->bind_param("ii", $request["leaderboardId"], $request["limit"]);
+        $stmt = $conn->prepare("SELECT SSO.Users.username AS 'username', SSO.Users.fullName AS 'fullName', SSO.Users.pfpPath AS 'pfpPath', `value` FROM Leaderboards_Data JOIN SSO.Users ON `user` = SSO.Users.id WHERE leaderboard = ? ORDER BY `value` " . $request["orderBy"] . (defined($LIMIT) ? " LIMIT ?" : ""));
+        if (defined($LIMIT)) $stmt->bind_param("ii", $request["leaderboardId"], $request["limit"]);
+        else $stmt->bind_param("i", $request["orderBy"]);
         $stmt->execute();
     
         $r = array();
@@ -54,8 +59,9 @@ if (array_key_exists("leaderboardId", $request)) {
 }
 else if (array_key_exists("leaderboardName", $request)) {
     try {
-        $stmt = $conn->prepare("SELECT SSO.Users.username AS 'username', SSO.Users.fullName AS 'fullName', SSO.Users.pfpPath AS 'pfpPath', `value` FROM Leaderboards_Data JOIN SSO.Users ON `user` = SSO.Users.id JOIN Leaderboards ON leaderboard = Leaderboards.id WHERE Leaderboards.`name` = ? ORDER BY `value` " . $request["orderBy"] . " LIMIT ?");
-        $stmt->bind_param("si", $request["leaderboardName"], $request["limit"]);
+        $stmt = $conn->prepare("SELECT SSO.Users.username AS 'username', SSO.Users.fullName AS 'fullName', SSO.Users.pfpPath AS 'pfpPath', `value` FROM Leaderboards_Data JOIN SSO.Users ON `user` = SSO.Users.id JOIN Leaderboards ON leaderboard = Leaderboards.id WHERE Leaderboards.`name` = ? ORDER BY `value` " . $request["orderBy"] . (defined($LIMIT) ? " LIMIT ?" : ""));
+        if (defined($LIMIT)) $stmt->bind_param("si", $request["leaderboardName"], $request["limit"]);
+        else $stmt->bind_param("s", $request["leaderboardName"]);
         $stmt->execute();
     
         $r = array();
