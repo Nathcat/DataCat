f38368e946a76c85df47e6048fa2d005633ed35e
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-03-28 17:33:48 +0000

    Set CORS header origin in get-session

--- sso/get-session.php
+++ sso/get-session.php
@@ -2,7 +2,7 @@
 include("start-session.php");
 
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: https://nathcat.net:8080");
+header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
