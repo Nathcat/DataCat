d8c2cde96bcf943d556d98afad3f2ff0f82ecd77
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-09-03 14:16:48 +0100

    Another fix to decline invite endpoint (passing token as wrong type + incorrect session variable name)

--- data/decline-group-invite.php
+++ data/decline-group-invite.php
@@ -25,7 +25,7 @@ if ($conn->connect_error) {
 
 try {
     $stmt = $conn->prepare("call decline_group_invite(?, ?)");
-    $stmt->bind_param("si", $request["token"], $session["user"]["id"]);
+    $stmt->bind_param("si", $request["token"], $_SESSION["user"]["id"]);
     
     if ($stmt->execute()) {
         echo json_encode(["status" => "success"]);
