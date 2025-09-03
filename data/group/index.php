406b2f4d9c709659c6b81bfa124f8a34129aa077
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-09-03 15:09:52 +0100

    Removed invite header from group view page

--- data/group/index.php
+++ data/group/index.php
@@ -1,66 +1,6 @@
 <?php
 include("../../../start-session.php");
-
-if (!array_key_exists("token", $_GET)) {
-    $ERR_MSG = "You must provide an invite token!";
-    include("__error.php");
-    exit();
-}
-
-$TOKEN = $_GET["token"];
-
-include("../start-session.php");
-
-if (!array_key_exists("user", $_SESSION)) {
-    $ERR_MSG = "You are not logged in!";
-    include("__error.php");
-    exit();
-}
-
-$conn = new mysqli("localhost:3306", "Data", "", "DataCat");
-
-if ($conn->connect_error) {
-    $ERR_MSG = "Failed to connect to database!";
-    include("__error.php");
-    exit();
-}
-
-try {
-    $stmt = $conn->prepare("SELECT `Groups`.`name` as 'name', SSO.Users.username as 'username', SSO.Users.fullName as 'fullName', SSO.Users.pfpPath as 'pfpPath' FROM `Group_Invites` JOIN `Groups` ON `Group_Invites`.`group` = `Groups`.`id` JOIN SSO.Users ON `Group_Invites`.`inviter` = SSO.Users.id WHERE `token` = ? AND `invitee` = ?;");
-    $stmt->bind_param("si", $TOKEN, $_SESSION["user"]["id"]);
-    $stmt->execute();
-
-    $r = array();
-    $set = $stmt->get_result();
-    while ($row = $set->fetch_assoc()) {
-        array_push($r, $row);
-    }
-
-    if (count($r) == 0) {
-        $ERR_MSG = "The specified invite could not be found, or you do not have access to it!";
-        include("__error.php");
-        $conn->close();
-        exit();
-    }
-    else if (count($r) != 1) {
-        $ERR_MSG = "An invalid number of invites was returned from the query! (" . count($r) . ")";
-        include("__error.php");
-        $conn->close();
-        exit();
-    }
-
-} catch (Exception $e) {
-    $conn->close();
-    $ERR_MSG = "Error while searching for invite! $e";
-    include("__error.php");
-    exit();
-}
-
-$conn->close();
-
-$invite = $r[0];
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -212,10 +152,8 @@ $invite = $r[0];
 
 
                 $conn->close();
-                
-                
+            
                 ?>
-
 
             <?php endif; ?>
 
