9bb02c64c725574ebdf55c895b4dadc4c70fba4c
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-09-05 22:35:22 +0100

    Added endpoint to remove user from group

---
+++ data/remove-from-group.php
@@ -0,0 +1,46 @@
+<?php
+include("../start-session.php");
+
+header("Content-Type: application/json");
+header("Accept: application/json");
+
+if (!array_key_exists("user", $_SESSION)) {
+    die("{\"status\": \"fail\", \"message\": \"Not logged in.\"}");
+}
+
+$request = json_decode(file_get_contents("php://input"), true);
+
+if (!array_key_exists("user", $request) || array_key_exists("group", $request)) {
+    die(json_encode([
+        "status" => "fail",
+        "message" => "You must specify the user ID and group ID"
+    ]));
+}
+
+$conn = new mysqli("localhost:3306", "Data", "", "DataCat");
+
+if ($conn->connect_error) {
+    die("{\"status\": \"fail\", \"message\": \"Failed to connect to the database: " . $conn->connect_error . "\"}");
+}
+
+try {
+    $stmt = $conn->prepare("call remove_from_group(?, ?, ?)");
+    $stmt->bind_param("iii", $request["group"] , $request["user"], $_SESSION["user"]["id"]);
+    
+    if ($stmt->execute()) {
+        echo json_encode(["status" => "success"]);
+    }
+    else {
+        echo json_encode(["status" => "fail", "message" => $conn->error]);
+    }
+
+} catch (Exception $e) {
+    $conn->close();
+    die("{\"status\": \"fail\", \"message\": \"$e\"}");
+}
+
+
+$conn->close();
+
+
+?>
