a4dee8529da3ee352d149da7e518d8d0c0742ded
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-09-25 22:53:46 +0100

    Simplification of quick auth endpoint

--- sso/create-quick-auth.php
+++ sso/create-quick-auth.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Credentials: true");
 
 include("start-session.php");
 
-if (array_key_exists("by-session", $_GET) && array_key_exists("user", $_SESSION)) {
+if (array_key_exists("user", $_SESSION)) {
     $conn = new mysqli("localhost:3306", "sso", "", "SSO");
     $stmt = $conn->prepare("CALL create_quick_auth(?)");
     $stmt->bind_param("i", $_SESSION["user"]["id"]);
@@ -20,52 +20,18 @@ if (array_key_exists("by-session", $_GET) && array_key_exists("user", $_SESSION)
 
 $request = json_decode(file_get_contents("php://input"), true);
 
-if (array_key_exists("DEBUG", $_GET)) {
-    echo "<p>In debug mode.</p>";
-    print_r($request);
-}
-
 $DB_server = "localhost:3306";
 $DB_user = "sso";
 $DB_pass = "";
 $DB_schema = "SSO";
 
-if (array_key_exists("quick-auth-token", $request)) {
-
-    try {
-        $conn = new mysqli($DB_server, $DB_user, $DB_pass, $DB_schema);
-        $stmt = $conn->prepare("SELECT Users.* FROM QuickAuth JOIN Users on Users.id = QuickAuth.id WHERE tokenHash = SHA2(?, 256)");
-        $stmt->bind_param("s", $request["quick-auth-token"]);
-        $stmt->execute(); $set = $stmt->get_result();
-
-        $res = $set->fetch_assoc();
-        if ($res !== NULL) {
-            $_SESSION["user"] = $DB_r;
-            unset($_SESSION["login-error"]);
-            $stmt->close();
-            $conn->close();
-            die("{\"status\": \"success\", \"user\": " . json_encode($res) . "}");
-        }
-
-        $stmt->close();
-        $conn->close();
-    }
-    catch (Exception $e) {
-        echo $e;
-    }
-}
-
-if (!(array_key_exists("username", $request) && array_key_exists("password", $request))) {
+if (array_key_exists("username", $request) || array_key_exists("password", $request)) {
     die("{\"status\": \"fail\", \"message\": \"Invalid request.\"}");
 }
 
 else if ($request["username"] == "" || $request["password"] == "") {
     $_SESSION["login-error"] = "Please provide both username and password";
     die("{\"status\": \"fail\", \"message\": \"Please provide both username and password.\"}");
-}
-
-if (array_key_exists("DEBUG", $_GET)) {
-    echo "<p>Username: " . $request["username"] . "<br>Password: " . $request["password"] . "</p>"; 
 }
 
 $conn = new mysqli($DB_server, $DB_user, $DB_pass, $DB_schema);
@@ -81,7 +47,7 @@ $stmt->execute();
 $result = $stmt->get_result();
 $DB_r = $result->fetch_assoc();
 
-if (!$DB_r["passwordUpdated"]) {
+if ($DB_r["passwordUpdated"] == 0) {
     die("{\"status\": \"fail\", \"message\": \"You must have updated your password to the new system to use this feature.\"}");
 }
 else if (password_verify($request["password"], $DB_r["password"])) {
