14b7c37439bae18539a42d087569918419804535
Nathan Baines <nathan.baines@nathcat.net>
2025-10-17 15:17:40 +0100

    Update change password endpoint

--- sso/change-password.php
+++ sso/change-password.php
@@ -4,27 +4,27 @@ include("start-session.php");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Accept: application/json");
 
-if ($_SERVER["CONTENT_TYPE"] == "application/json") {
-    $_POST = json_decode(file_get_contents("php://input"), true);
-}
+$request = json_decode(file_get_contents("php://input"), true);
 
-if (array_key_exists("DEBUG", $_GET)) {
-    echo "<p>In debug mode.</p>";
-    print_r($_POST);
-}
-
-if (array_key_exists("user", $_SESSION) && array_key_exists("password", $_POST)) {
+if (array_key_exists("user", $_SESSION) && array_key_exists("password", $request)) {
     $conn = new mysqli("localhost:3306", "sso", "", "SSO");
-    $stmt = $conn->prepare("UPDATE Users SET password = ?, passwordUpdated = 1 WHERE id = ?");
+    $stmt = $conn->prepare("UPDATE Users SET password = ? WHERE id = ?");
     $stmt->bind_param("sd", password_hash($_POST["password"], PASSWORD_DEFAULT), $_SESSION["user"]["id"]);
     $stmt->execute();
     $stmt->close();
     $conn->close();
 }
 else {
-    die("Missing data!");
+    die(json_encode([
+        "status" => "fail",
+        "message" => "Missing data"
+    ]));
 }
 
 session_destroy();
+echo json_encode([
+    "status" => "success"
+]);
 ?>
