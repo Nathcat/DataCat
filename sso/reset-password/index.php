859a9924506f80a6496c1dee8dc4c092f4422f84
Nathan Baines <nathan.baines@nathcat.net>
2025-10-29 14:52:29 +0000

    Changed redirect link and added message before redirect

--- sso/reset-password/index.php
+++ sso/reset-password/index.php
@@ -54,7 +54,7 @@
                             <div class="content-card column">
                                 <input id="pass1" type="password" placeholder="New password..." />
                                 <input id="pass2" type="password" placeholder="New password again..." />
-                                <button onclick="sso_reset_password('<?php echo $_GET['t']; ?>', $('#pass1').val(), $('#pass2').val(), (r) => { location = 'https://data.nathcat.net'; }, alert)">Submit new password</button>
+                                <button onclick="sso_reset_password('<?php echo $_GET['t']; ?>', $('#pass1').val(), $('#pass2').val(), (r) => { alert('Password reset successfully!'); location = 'https://data.nathcat.net/sso'; }, alert)">Submit new password</button>
                             </div>
                         <?php
                     else :
