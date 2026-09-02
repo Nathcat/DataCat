f3c107f0ccec786cd978748b69f1c249d3df6269
Nathan Baines <nathan.baines@nathcat.net>
2026-09-02 16:45:33 +0100

    apiserver and cmake setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
-#include <DataCat/DataCat.hpp>
+#include "api/api.hpp"
+#include <httplib.h>
 
-int main() {}
+void f(const httplib::Request &req, httplib::Response &res) {
+  res.status = httplib::StatusCode::OK_200;
+  res.set_content("Hello world :3", "text/plain");
+}
+
+int main() {
+  nathcat::api::Server server;
+  server.registerEndpoint({"/", {f, nullptr}});
+
+  server.listen("0.0.0.0", 9090);
+}
