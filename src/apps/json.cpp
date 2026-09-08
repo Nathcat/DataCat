51fbd1a5a1add35c63caa759b0919034e8334ab8
Nathan Baines <nathan.baines@nathcat.net>
2026-09-08 18:20:23 +0100

    de-init project

--- src/apps/json.cpp
+++
@@ -1,6 +0,0 @@
-#include <DataCat/Apps.hpp>
-
-void nathcat::data::apps::to_json(nlohmann::json &j, const struct App &a) {
-  j = {
-      {"id", a.id}, {"owner", a.owner}, {"name", a.name}, {"apiKey", a.apiKey}};
-}
